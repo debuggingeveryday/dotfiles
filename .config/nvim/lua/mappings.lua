a0281087c9b857d6303507650dc2f230e833d294
scarfaceshing <edrianjansolatorio@gmail.com>
2026-06-03 20:53:39 +0000

    fixed

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,6 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Start Pomodoro
 map("n", "<F12>", "<cmd> TimerSession pomodoro<cr>", { desc = "Pomodoro Start" })
 
 map("n", "K", "Eli<CR><Esc>", { desc = "Enter next line" })
